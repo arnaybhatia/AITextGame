7c3d9f484efcb04a1fd8fd9377c20dc440645336
strifedeeno <coolarnay@gmail.com>
2024-12-03 11:56:30 -0800

    Add user name input functionality with button to change user

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,31 @@ myImage.onclick = () => {
     console.log("logo");
   }
 };
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+myButton.onclick = () => {
+  setUserName();
+};
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
