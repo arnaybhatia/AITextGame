02f0080e0436b6153fbfa90e9014c1fcbdc7db21
strifedeeno <coolarnay@gmail.com>
2024-12-03 11:54:06 -0800

    Update image toggle functionality to switch between logo and derpy Woody

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,12 @@ const myImage = document.querySelector("img");
 
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
+  console.log("click recieved");
+  if (mySrc === "images/logo.webp") {
     myImage.setAttribute("src", "images/derpyWoody.jpeg");
+    console.log("derpy woody");
   } else {
     myImage.setAttribute("src", "images/logo.webp");
+    console.log("logo");
   }
 };
